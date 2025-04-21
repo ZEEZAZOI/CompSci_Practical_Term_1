baf1da7ba664d6b03a7e411234949772c7b74404
Karim Hegazi <karimhegazi2008@gmail.com>
2025-04-21 22:09:13 +0800

    Just added The Display function's first version

--- Database.py
+++ Database.py
@@ -3,6 +3,16 @@ class Warehouse:
     def __init__(self, Xsize, Ysize):
         self.Aisles = Xsize
         self.Rows = Ysize
+    def Display(self):
+    #Making it into a 2D array
+        TableArray = []
+        for j in range(self.Rows):
+            TableArray.append()
+            for i in range(self.Aisles):
+                TableArray[j](" X ")
+        #Displaying it using a print Statement
+        for j in range(len(TableArray[j])):
+            print(TableArray[j])
 #Location Class
 class Location:
     def __init__(self, Xposition, Yposition):
@@ -14,3 +24,6 @@ class Product:
         self.Name = Name
         self.Quantity = Quantity
         self.Price = Price
+# First Make the Warehouse Size and display
+warehouse1 = Warehouse(10,10)
+print(warehouse1.Rows)
